acc9ba74eb41dd1626d721e332ab2bf80efb70d8
Moisés Mariano <moisesduartem@gmail.com>
2022-02-05 15:09:39 -0300

    validating account

--- src/index.js
+++ src/index.js
@@ -28,6 +28,12 @@ app.get('/statements/:cpf', (request, response) => {
 
     const customer = customers.find(customer => customer.cpf === cpf);
 
+    if (!customer) {
+        return response.status(400).json({
+            error: 'Customer not found'
+        });
+    }
+
     return response.json(customer.statement);
 });
 
