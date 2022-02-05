e321edaefe437d2056d8d435a6ea947fcbb5da7c
Moisés Mariano <moisesduartem@gmail.com>
2022-02-05 14:48:31 -0300

    using uuid & creating an account

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
+const customers = [];
+
 app.use(express.json());
+
+app.post('/accounts', (request, response) => {
+    const { cpf, name } = request.body;
+
+    const id = uuidv4();
+
+    customers.push({ id, cpf, name, statement: [] });
+
+    return response.status(201).send();
+});
 
 app.listen(3333);
