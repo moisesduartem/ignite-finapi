7746a7976e877e59c5a06e0f540079371242d64c
Moisés Mariano <moisesduartem@gmail.com>
2022-02-05 15:29:23 -0300

    making deposit

--- src/index.js
+++ src/index.js
@@ -44,4 +44,21 @@ app.get('/statements', verifyIfExistsAccountCPF, (request, response) => {
     return response.json(customer.statement);
 });
 
+app.post('/deposit', verifyIfExistsAccountCPF, (request, response) => {
+    const { description, amount } = request.body;
+
+    const { customer } = request;
+
+    const statementOperation = {
+        amount,
+        description,
+        created_at: new Date(),
+        type: "credit"
+    };
+
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send();
+})
+
 app.listen(3333);
